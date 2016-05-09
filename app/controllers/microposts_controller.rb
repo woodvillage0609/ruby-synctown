2f06735bc143fe954228a03c96b2b7778d64aadd
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-09 22:29:58 +0100

    article and micropost super user

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -74,7 +74,7 @@ class MicropostsController < ApplicationController
     end
 
     def super_user
-      unless user_signed_in? && current_user.id == 1 
+      unless user_signed_in? && current_user.email == "woodvillage0609@gmail.com" 
         redirect_to root_path
       end
     end
