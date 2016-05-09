5dd4160be67c52c54e867d2bbea5c0514b0095e2
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-09 22:05:47 +0100

    changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,3 @@
 module UsersHelper
 
-def image_for(user)
-	
-	if user.image
-		image_tag "/user_images/#{user.image}", class: "img-circle"
-	else 
-		image_tag "IMG_0058.jpg"
-	end
-    end
-
 end
