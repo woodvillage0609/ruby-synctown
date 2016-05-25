cadb1040e81fd99eb190747ce190d0b993347d3e
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-25 22:53:06 +0100

    improvements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,13 @@
 module UsersHelper
 
+	def image_for(user)
+
+		if user.image?
+			image_tag(user.image, {class: "img-circle"})
+		else
+			image_tag("IMG_0121.jpg", {class: "img-circle"})
+		end
+
+	end
+
 end
