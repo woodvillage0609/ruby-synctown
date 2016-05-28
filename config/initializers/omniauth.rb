794546f351429916d2b9c2ca63d7534e2c7ca7a4
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-28 23:50:30 +0100

    facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,1 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-end
+OmniAuth.config.logger = Rails.logger
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,7 @@ Rails.application.routes.draw do
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  devise_for :users, :controllers => { :registration => "registration", :omniauth_callbacks => "omniauth_callbacks" }
 
   resources :users, only:[:index, :show, :edit, :update] do
     member do 
