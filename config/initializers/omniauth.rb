c7b8122ac083a437ac605f90b98f4024df8ad2a2
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-28 23:54:37 +0100

    cahnges

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:provider_ignores_state => true}
+end
