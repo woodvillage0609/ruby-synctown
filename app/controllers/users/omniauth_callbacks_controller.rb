1f6d228c2362a63fecb45e503807ead7b8e01471
woodvillage0609 <woodvillage0609@gmail.com>
2016-06-06 14:25:29 +0100

    changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,8 +13,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ActiveRecord::Base
     user.uid   = auth.uid
     user.name  = auth.info.name
     user.email = auth.info.email
-    user.icon  = auth.info.image
+    user.image  = auth.info.image
     user.oauth_token      = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
     user
