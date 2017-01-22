825e138632d498f2c3fec947754a823f6c8162e8
woodvillage0609 <woodvillage0609@gmail.com>
2017-01-22 20:18:11 +0900

    image changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,10 @@ class User < ActiveRecord::Base
   has_many :opinions
   has_many :opinion_microposts, through: :opinions, source: :micropost
 
-  has_attached_file :image
+
+  has_attached_file :image, :styles => { :thumb => "800x>"}
+
+
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 # ユーザーをフォローする
